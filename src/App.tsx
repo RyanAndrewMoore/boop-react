fb7ee4897599ff318c61c0db25e63f77ec54abc8
Ryan Andrew Moore <67835212+RyanAndrewMoore@users.noreply.github.com>
2026-07-12 22:53:59 -0700

    Move pieces from hand to board and cycle turns
    Pieces are selected, placed on board, removed from hand, and turns cycle
    properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,174 @@
+import { useState, type MouseEventHandler } from "react";
 import "./App.css";
 
 type Piece = "🐈" | "🐱" | "🐅" | "🐯";
+const [KITTEN, CAT, CUB, TIGER]: Piece[] = ["🐈", "🐱", "🐅", "🐯"];
 
-const [kitten, cat, cub, tiger]: Piece[] = ["🐈", "🐱", "🐅", "🐯"];
-
-function Square({ piece }: { piece?: Piece }) {
+function Square({
+  piece,
+  toggleSelected,
+  onSquareClick,
+}: {
+  piece?: Piece;
+  toggleSelected?: boolean;
+  onSquareClick?: MouseEventHandler;
+}) {
   return (
     <div className="square">
-      <button>{piece}</button>
+      <button
+        style={
+          toggleSelected ? { backgroundColor: "var(--accent-border)" } : {}
+        }
+        onClick={onSquareClick}
+      >
+        {piece}
+      </button>
     </div>
   );
 }
 
-function Board() {
-  const pieces: Piece[] = [kitten,cat,cub,tiger]
-  const rows = [];
-  for (let i = 0; i < 6; i++) {
-    const row = [];
-    for (let j = 0; j < 6; j++) {
-      const index = Math.round(Math.random() * 3)
-      row.push(<Square piece={pieces[index]} />);
-    }
+function Board({
+  squares,
+  onPlay,
+  selected,
+}: {
+  squares?: Piece[][];
+  onPlay: Function;
+  selected: { piece: Piece; index: number };
+}) {
+  function handleClick(row: number, col: number) {
+    if (selected.index < 0 || !squares) return;
 
-    rows.push(row);
+    const nextSquares = squares.map(row => row.slice())
+    nextSquares[row][col] = selected.piece;
+    onPlay(nextSquares);
   }
+  return (
+    <div className="game-board">
+      {squares?.map((row, rowIndex) => {
+        if (rowIndex > 0 && rowIndex < squares.length - 1) {
+          return row.map((square, colIndex) => {
+            if (colIndex > 0 && colIndex < row.length - 1) {
+              return (
+                <Square
+                  key={rowIndex * 8 + colIndex}
+                  piece={square}
+                  onSquareClick={() => handleClick(rowIndex, colIndex)}
+                />
+              );
+            }
+          });
+        }
+      })}
+    </div>
+  );
+}
 
-  return <div className="game-board">{rows}</div>;
+function Hand({
+  pieces,
+  player,
+  catsAreNext,
+  onSelect,
+  selected,
+}: {
+  pieces?: Piece[];
+  player: "cats" | "tigers";
+  catsAreNext: boolean;
+  onSelect: (piece: Piece, index: number) => void;
+  selected: { piece: Piece; index: number };
+}) {
+  const hand: React.JSX.Element[] = [];
+  const isPlayerTurn =
+    (player === "cats" && catsAreNext) || (player === "tigers" && !catsAreNext);
+
+  const selectFunc: (piece: Piece, index: number) => void = isPlayerTurn ? onSelect : () => null;
+
+  {
+  }
+  pieces?.forEach((piece, index) =>
+    hand.push(
+      <Square
+        key={index}
+        piece={piece}
+        onSquareClick={() => selectFunc(piece, index)}
+        toggleSelected={isPlayerTurn && (index === selected.index) ? true : false}
+      />,
+    ),
+  );
+
+  return hand;
 }
 
 function App() {
+  const [squares, setSquares]: [
+    Piece[][],
+    React.Dispatch<React.SetStateAction<Piece[][]>>,
+  ] = useState(Array(8).fill(Array(8).fill(null)));
+  const [pieces, setPieces] = useState({
+    kittens: 4,
+    cats: 0,
+    cubs: 4,
+    tigers: 0,
+  });
+  const [catsAreNext, setCatsAreNext] = useState(true);
+  const hands = calcHands();
+  const catHand: Piece[] = Array(hands.kittens)
+    .fill(KITTEN)
+    .concat(Array(hands.cats).fill(CAT));
+  const tigerHand: Piece[] = Array(hands.cubs)
+    .fill(CUB)
+    .concat(Array(hands.tigers).fill(TIGER));
+  const [selected, setSelected] = useState({ piece: KITTEN, index: -1 });
+
+  function handlePlay(squares: Piece[][]) {
+    setSquares(squares);
+    setSelected({piece: KITTEN,index: -1})
+    setCatsAreNext(!catsAreNext)
+  }
+
+  function handleSelect(piece: Piece, index: number ) {
+    setSelected({piece: piece, index: index});
+  }
+
+  function countBoardPieces(character: Piece) {
+    return squares.flat().filter((square) => square === character).length;
+  }
+
+  function calcHands() {
+    return {
+      kittens: pieces.kittens - countBoardPieces(KITTEN),
+      cats: pieces.cats - countBoardPieces(CAT),
+      cubs: pieces.cubs - countBoardPieces(CUB),
+      tigers: pieces.tigers - countBoardPieces(TIGER),
+    };
+  }
+
   return (
     <>
-      <Board />
+      <h1>Boop</h1>
+      <div className="game">
+        <Board squares={squares} selected={selected} onPlay={handlePlay} />
+        <div className="hands">
+          <div className="cat-hand">
+            <Hand
+              pieces={catHand}
+              catsAreNext={catsAreNext}
+              player="cats"
+              onSelect={handleSelect}
+              selected={selected}
+            />
+          </div>
+          <div className="tiger-hand">
+            <Hand
+              pieces={tigerHand}
+              catsAreNext={catsAreNext}
+              player="tigers"
+              onSelect={handleSelect}
+              selected={selected}
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
