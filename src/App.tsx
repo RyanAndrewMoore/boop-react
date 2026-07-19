985a96aa30f1fb2a8703d0238e48366f1f58f0d4
Ryan Andrew Moore <67835212+RyanAndrewMoore@users.noreply.github.com>
2026-07-19 09:48:36 -0700

    Working boop logic for small pieces
    Pieces boop each other with correct logic, i.e.: if there is space at
    the destination, piece is moved to destination, unless destination is
    in the gutter, then piece is removed from board. No large pieces can
    yet enter the game (promotion not yet implemented) and so logic for
    large pieces booping and being booped is not yet implemented.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import { useState, type MouseEventHandler } from "react";
 import "./App.css";
 
-type Piece = "🐈" | "🐱" | "🐅" | "🐯";
+type Piece = "🐈" | "🐱" | "🐅" | "🐯" | null;
 const [KITTEN, CAT, CUB, TIGER]: Piece[] = ["🐈", "🐱", "🐅", "🐯"];
 
 function Square({
   piece,
   toggleSelected,
   onSquareClick,
+  invisible,
 }: {
   piece?: Piece;
   toggleSelected?: boolean;
   onSquareClick?: MouseEventHandler;
+  invisible?: boolean;
 }) {
+  const style: React.CSSProperties = {};
+
+  if (toggleSelected) style.backgroundColor = "var(--accent-border)";
+  if (invisible) style.visibility = "hidden";
+
   return (
     <div className="square">
-      <button
-        style={
-          toggleSelected ? { backgroundColor: "var(--accent-border)" } : {}
-        }
-        onClick={onSquareClick}
-      >
+      <button style={style} onClick={onSquareClick}>
         {piece}
       </button>
     </div>
@@ -32,33 +34,35 @@ function Board({
   onPlay,
   selected,
 }: {
-  squares?: Piece[][];
+  squares?: Piece[][] | null[][];
   onPlay: Function;
   selected: { piece: Piece; index: number };
 }) {
   function handleClick(row: number, col: number) {
-    if (selected.index < 0 || !squares) return;
+    if (selected.index < 0 || !squares || squares[row][col]) return;
 
-    const nextSquares = squares.map(row => row.slice())
+    const nextSquares = squares.map((row) => row.slice());
     nextSquares[row][col] = selected.piece;
-    onPlay(nextSquares);
+    onPlay(row, col, nextSquares);
   }
   return (
     <div className="game-board">
       {squares?.map((row, rowIndex) => {
-        if (rowIndex > 0 && rowIndex < squares.length - 1) {
-          return row.map((square, colIndex) => {
-            if (colIndex > 0 && colIndex < row.length - 1) {
-              return (
-                <Square
-                  key={rowIndex * 8 + colIndex}
-                  piece={square}
-                  onSquareClick={() => handleClick(rowIndex, colIndex)}
-                />
-              );
-            }
-          });
-        }
+        return row.map((square, colIndex) => {
+          return (
+            <Square
+              key={rowIndex * 8 + colIndex}
+              piece={square}
+              onSquareClick={() => handleClick(rowIndex, colIndex)}
+              invisible={
+                rowIndex == 0 ||
+                rowIndex == squares.length - 1 ||
+                colIndex == 0 ||
+                colIndex == squares.length - 1
+              }
+            />
+          );
+        });
       })}
     </div>
   );
@@ -81,7 +85,9 @@ function Hand({
   const isPlayerTurn =
     (player === "cats" && catsAreNext) || (player === "tigers" && !catsAreNext);
 
-  const selectFunc: (piece: Piece, index: number) => void = isPlayerTurn ? onSelect : () => null;
+  const selectFunc: (piece: Piece, index: number) => void = isPlayerTurn
+    ? onSelect
+    : () => null;
 
   {
   }
@@ -91,7 +97,7 @@ function Hand({
         key={index}
         piece={piece}
         onSquareClick={() => selectFunc(piece, index)}
-        toggleSelected={isPlayerTurn && (index === selected.index) ? true : false}
+        toggleSelected={isPlayerTurn && index === selected.index ? true : false}
       />,
     ),
   );
@@ -101,8 +107,8 @@ function Hand({
 
 function App() {
   const [squares, setSquares]: [
-    Piece[][],
-    React.Dispatch<React.SetStateAction<Piece[][]>>,
+    Piece[][] | null[][],
+    React.Dispatch<React.SetStateAction<Piece[][] | null[][]>>,
   ] = useState(Array(8).fill(Array(8).fill(null)));
   const [pieces, setPieces] = useState({
     kittens: 4,
@@ -120,14 +126,63 @@ function App() {
     .concat(Array(hands.tigers).fill(TIGER));
   const [selected, setSelected] = useState({ piece: KITTEN, index: -1 });
 
-  function handlePlay(squares: Piece[][]) {
+  function handlePlay(
+    moveRow: number,
+    moveCol: number,
+    squares: [[Piece | null]],
+  ) {
+    const movePiece = squares[moveRow][moveCol];
+    const blastZone = [];
+
+    // blastZone should be all pieces adjacent and diagonal to move
+    for (let i = moveRow - 1; i < moveRow + 2; i++) {
+      for (let j = moveCol - 1; j < moveCol + 2; j++) {
+        if (i == moveRow && j == moveCol) continue
+        blastZone.push([i,j])
+      }
+    }
+
+    blastZone.forEach((target) => boop(moveRow, moveCol, target[0], target[1]));
+
+    function boop(
+      moveRow: number,
+      moveCol: number,
+      targetRow: number,
+      targetCol: number,
+    ) {
+      if (isPieceInGutter(targetRow,targetCol)) return
+      // (targetRow - moveRow) + targetRow, algebraically reduced
+      const destinationRow = 2 * targetRow - moveRow;
+      const destinationCol = 2 * targetCol - moveCol;
+
+      if (squares[destinationRow][destinationCol]) return;
+
+      if (!isPieceInGutter(destinationRow, destinationCol)) {
+        squares[destinationRow][destinationCol] = squares[targetRow][targetCol];
+      }
+      squares[targetRow][targetCol] = null;
+    }
+
+    function isPieceInGutter(row: number, col: number) {
+      return (
+        row == 0 ||
+        row == squares.length - 1 ||
+        col == 0 ||
+        col == squares.length - 1
+      );
+    }
+
+    function isPieceLarge(piece: Piece): boolean {
+      return [CAT, TIGER].includes(piece) ? true : false;
+    }
+
     setSquares(squares);
-    setSelected({piece: KITTEN,index: -1})
-    setCatsAreNext(!catsAreNext)
+    setSelected({ piece: KITTEN, index: -1 });
+    setCatsAreNext(!catsAreNext);
   }
 
-  function handleSelect(piece: Piece, index: number ) {
-    setSelected({piece: piece, index: index});
+  function handleSelect(piece: Piece, index: number) {
+    setSelected({ piece: piece, index: index });
   }
 
   function countBoardPieces(character: Piece) {
